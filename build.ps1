edc3db4b580922c7baddb5164f1d965eb83e8cbc
MURATA Mitsuharu <hikari.photon+dev@gmail.com>
2025-01-02 21:28:44 +0900

    いろいろ整理

---
+++ build.ps1
@@ -0,0 +1,9 @@
+﻿# クライアントのビルド
+if (Test-Path build) {
+    Remove-Item -Recurse -Force build
+}
+New-Item -ItemType Directory -Path build
+Set-Location client/demo
+yarn build:prod
+Set-Location ..\..
+Copy-Item -Recurse client\demo\dist build\client
